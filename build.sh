af12b50d920976a43fd25b07b36ae271f7a1ffef
Indrajit Sarkar <45512567+indraskr9@users.noreply.github.com>
2018-12-29 01:15:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,4 @@ prebuilts/misc/linux-x86/ccache/ccache -M 100G
 lunch dot_aio_otfp-userdebug
 export KBUILD_BUILD_USER=Indraskr
 export KBUILD_BUILD_HOST=indraskr
-export ALLOW_MISSING_DEPENDENCIES=true
-export CUSTOM_BUILD_TYPE=OFFICIAL
 mka bacon -j32
